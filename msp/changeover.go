9698d90308e34702f3212a386b1b7087fef3f9d4
Ethan Holz <iplaybassx2@gmail.com>
2022-10-13 17:02:55 -0500

    feat: Initial changes to handline generating a timeline

--- msp/changeover.go
+++ msp/changeover.go
@@ -1,6 +1,7 @@
 package msp
 
 import (
+	"fmt"
 	"sort"
 	"time"
 )
@@ -57,4 +58,27 @@ func FlattenPeriods(periods ...Period) (ids []string) {
 		ids = append(ids, id)
 	}
 	return
+}
+
+func GenerateTimeline(periods ...Period) (out []string) {
+	periodsByID := make(map[string]Period)
+	ids := FlattenPeriods(periods...)
+	for _, val := range periods {
+		id := val.GetIdentifier()
+		periodsByID[id] = val
+	}
+	start := periodsByID[ids[0]].GetStartTime()
+	for _, val := range ids {
+		next, err := GetNextChangeOver(start, periods...)
+		if err == nil {
+			if next.Equal(periodsByID[val].GetStartTime()) {
+				start = periodsByID[val].GetStartTime()
+				next = periodsByID[val].GetEndTime()
+			}
+			frame := fmt.Sprintf("%s\t%s\t%s\n", val, start, next)
+			out = append(out, frame)
+			start = next
+		}
+	}
+	return out
 }
